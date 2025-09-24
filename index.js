fcfa2204daaff5118270484992d6f860af26d997
Your Name <raghurajramkumar927@gmail.com>
2025-09-24 21:39:25 +0530

    Initial commit

--- index.js
+++ index.js
@@ -99,6 +99,7 @@ async function getAuthenticatedClient() {
         return null;
     }
 
+    
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
